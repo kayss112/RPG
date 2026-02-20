cf2df70b5eadefe5f9c3d6e716681bc3753c380e
Jie Wen <3128879978@qq.com>
2026-02-20 21:57:40 +0900

    first

---
+++ Source/Aura/Private/Character/AuraCharacterBase.cpp
@@ -0,0 +1,22 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Character/AuraCharacterBase.h"
+
+// Sets default values
+AAuraCharacterBase::AAuraCharacterBase()
+{
+	PrimaryActorTick.bCanEverTick = false;
+
+	weapon = CreateDefaultSubobject<USkeletalMeshComponent>("Weapon");
+	weapon->SetupAttachment(GetMesh(), "WeaponHandSocket");
+	weapon->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+}
+
+// Called when the game starts or when spawned
+void AAuraCharacterBase::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
